c4a3edbdc799717cae373f2219cf7fa9bc49a7ee
xavier7115 <xscott2@students.kennesaw.edu>
2022-12-04 20:32:22 -0500

    Password reminder complete
    Successfully updated the code to print the password on a new line that signifies the user's current progress.
    
    This is done when the user says "no" to generating another password.

--- main copy.py
+++ main copy.py
@@ -39,9 +39,9 @@ for x in range(amount):
             
 
     else:
-        print(f'Thanks for using Secure Password Generator!')
+        print(f'Thanks for using Secure Password Generator!\nYour password is:')
         break
-print("Reminder, your password is:" (password))
+print((password))
 
 
         # updated the message to alert the user that the new password has been generated.
